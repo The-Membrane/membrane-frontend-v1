4f79955452bf08d37ed373cb568b4c438c601c07
triccs <EBmichigan99@gmail.com>
2023-09-06 20:39:08 -0400

    set progress precision to 3

--- pages/Lockdrop.tsx
+++ pages/Lockdrop.tsx
@@ -370,7 +370,7 @@ const Lockdrop = ({launch_client, queryClient, address, prices}: Props) => {
           var current_time = Date.parse(block.header.time) / 1000;
           
           //Calc & set progress
-          setProgress(parseFloat(((current_time - res.start_time) / (res.withdrawal_end - res.start_time)).toPrecision(4)) * 100);
+          setProgress(parseFloat(((current_time - res.start_time) / (res.withdrawal_end - res.start_time)).toPrecision(3)) * 100);
         })} );  
     } catch (error) {
       console.log(error);
