96a55d3d83d18a436536dcf5610973f58914a19d
triccs <EBmichigan99@gmail.com>
2024-01-01 17:49:50 -0500

    Change back to Lava RPC

--- hooks/use-clients.ts
+++ hooks/use-clients.ts
@@ -120,7 +120,7 @@ export function useQueryClients(): {
     //Reece's: osmosis-rpc.reece.sh
     //Polkachu's: https://osmosis-rpc.polkachu.com/
     //Meta: https://rpc.cosmos.directory/osmosis
-    var client = CosmWasmClient.connect("osmosis-rpc.reece.sh")
+    var client = CosmWasmClient.connect("https://g.w.lavanet.xyz:443/gateway/cos3/rpc-http/bb6d2019c50124ec4fdb78498bc50573")
     .catch((e) => {
       console.log(e);
       query_errored = true;
